8ae9c30a4ac54e428a2f80bd4f972904e677ca44
Emir || 2099 <emirsteveroger@gmail.com>
2025-11-12 08:35:54 +0530

    Enhance enemy combat and player movement: implement knockback effect and refine attack state management

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,31 +5,49 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5;
     public int facingDirection = 1; // 1: right, -1: left
+    private bool isKnockedBack;
     public Rigidbody2D rb;
     public Animator animt;
-
 
     // FixedUpdate is called 50x per frame
     private void FixedUpdate()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        if (horizontal > 0 && transform.localScale.x < 0 || horizontal < 0 && transform.localScale.x > 0)
+        if (isKnockedBack == false)
         {
-            Flip();
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            if (horizontal > 0 && transform.localScale.x < 0 || horizontal < 0 && transform.localScale.x > 0)
+            {
+                Flip();
+            }
+
+
+            animt.SetFloat("horizontal", Mathf.Abs(horizontal));
+            animt.SetFloat("vertical", Mathf.Abs(vertical));
+
+            rb.linearVelocity = new Vector2(horizontal * speed, vertical * speed);
         }
-
-
-        animt.SetFloat("horizontal", Mathf.Abs(horizontal));
-        animt.SetFloat("vertical", Mathf.Abs(vertical));
-
-        rb.linearVelocity = new Vector2(horizontal * speed, vertical * speed);
     }
-    
+
     void Flip()
     {
         facingDirection *= -1;
-        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);   
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+
+    public void Knockback(Transform enemy, float force, float stunTime)
+    {
+        isKnockedBack = true;
+        Vector2 direction = (transform.position - enemy.position).normalized;
+        rb.velocity = direction * force;
+        StartCoroutine(KnockbackCounter(stunTime));
+    }
+
+    IEnumerator KnockbackCounter(float stunTime)
+    {
+        yield return new WaitForSeconds(stunTime);
+        rb.velocity = Vector2.zero;
+        isKnockedBack = false;
     }
 }
